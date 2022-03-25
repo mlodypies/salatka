89003fa857de5df38b83fcb6ad145b9fb2f850cb
3110180 <3110180@TE-GDA-D-D26F05>
2022-03-25 14:43:05 +0100

    Dodaj pliki projektów.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Podaj wiadomosc do zakodowania");
+string litera = Console.ReadLine();
+char[] character = litera.ToCharArray();
+int b;
+Console.WriteLine("Przesun o");
+b = int.Parse(Console.ReadLine());
+for (int i = 0; i < character.Length; i++)
+{
+    character[i] = (char)((character[i] + b));
+    if (character[i] > 90)
+    {
+        character[i] = (char)((char)character[i] - 26);
+    }
+}
+string litera2 = new string(character);
+Console.WriteLine("Zakodowana wiadomosc to: ");
+Console.WriteLine(litera2);
